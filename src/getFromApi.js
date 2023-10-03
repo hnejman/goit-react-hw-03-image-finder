5d59d6f895101068a69967b96db9b2cc57bc8e07
hnejman <108231655+hnejman@users.noreply.github.com>
2023-10-03 17:54:34 +0200

    end

--- src/getFromApi.js
+++ src/getFromApi.js
@@ -3,10 +3,15 @@ import axios from 'axios';
 axios.defaults.baseURL = 'https://pixabay.com/api';
 
 async function getFromAPI(search, key, page) {
-    const response = await axios.get(
-      `?key=${key}&page=${page}&q=${search}&image_type=photo&orientation=horizontal&per_page=15`
-    );
+    try {
+      const response = await axios.get(
+        `?key=${key}&page=${page}&q=${search}&image_type=photo&orientation=horizontal&per_page=15`
+      );
     return response;
+    } catch (error) {
+      console.log(error);
+      alert(error);
+    } 
   }
 
 export default getFromAPI;
